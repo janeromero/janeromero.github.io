e2404de94894c6554c0811baa9030d1cc9d7f55b
Jane Romero <romerojaneruth@gmail.com>
2018-05-04 07:23:30 +0800

    new

---
+++ portfolio/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$(".fa-times").click(function(){
+		$(".sidebar-menu").addClass("hide-menu");
+		$(".toggle-menu").addClass("opacity-one");
+	});
+
+	$(".toggle-menu").click(function(){
+		$(".sidebar-menu").removeClass("hide-menu");
+		$(".toggle-menu").removeClass("opacity-one");
+	});
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "grey";
+};
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+};
+
+
